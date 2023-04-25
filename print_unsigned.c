c4be40683bf0597fc9ec71405914217dfa7828d2
Akwesi-bonah <arhinbonnah@gmail.com>
2023-04-25 10:51:18 +0000

    int

--- print_unsigned.c
+++ print_unsigned.c
@@ -8,39 +8,39 @@
 
 int print_unsigned_int(va_list arg)
 {
-    unsigned int a = va_arg(arg, unsigned int);
-    int num, i = 1;
-    int end = a % 10, digit, exp = 1;
+	unsigned int val = va_arg(arg, unsigned int);
+	int num, end = val % 10, digit, exp = 1;
+	int  i = 1;
 
-    a /= 10;
-    num = a;
-    if (end < 0)
-    {
-        _putchar('-');
-        num = -num;
-        a = -a;
-        end = -end;
-        i++;
-    } 
+	val = val / 10;
+	num = val;
 
-    if (num > 0)
-    {
-        while (num / 10 != 0)
-        {
-            exp = exp * 10;
-            num = num / 10;
-        }
-        num = a;
+	if (end < 0)
+	{
+		_putchar('-');
+		num = -num;
+		val = -val;
+		end = -end;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = val;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(end + '0');
 
-        while (exp > 0)
-        {
-            digit = num / exp;
-            _putchar(digit + '0');
-            num = num - (digit * exp);
-            exp = exp / 10;
-            i++;
-        }
-    }
-    _putchar(end + '0');
-    return (i);
+	return (i);
 }
