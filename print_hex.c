0d9f610ef427f03cbe6a6a6e0d17d2d2c4295605
rabs23 <ruthrabs22@gmail.com>
2023-04-25 02:53:45 -0700

    print_hex.c

--- print_hex.c
+++ print_hex.c
@@ -8,42 +8,42 @@
 
 int print_hex(va_list arg)
 {
-    int i, count = 0;
-    int *arr;
+	int i, count = 0;
+	int *arr;
 
-    unsigned int num = va_arg(arg, unsigned int);
-    unsigned int temp = num;
+	unsigned int num = va_arg(arg, unsigned int);
+	unsigned int temp = num;
 
-    while (num / 16 != 0)
-    {
-        num /= 16;
-        count++;
-    }
-    count++;
-    arr = malloc(sizeof(int) * (count));
+	while (num / 16 != 0)
+	{
+	num /= 16;
+	count++;
+	}
+	count++;
+	arr = malloc(sizeof(int) * (count));
 
-    i = 0;
-    while (i < count)
-    {
-        arr[i] = temp % 16;
-        temp /= 16;
-        i++;
-    }
+	i = 0;
+	while (i < count)
+	{
+	arr[i] = temp % 16;
+	temp /= 16;
+	i++;
+	}
 
-    i = (count - 1);
+	i = (count - 1);
 
-    while (i >= 0)
-    {
-        if (arr[i] > 9)
-        {
-            arr[i] = arr[i] + 39;
-        }
-        _putchar(arr[i] + '0');
-        i--;
-    }
+	while (i >= 0)
+	{
+	if (arr[i] > 9)
+	{
+		arr[i] = arr[i] + 39;
+	}
+	_putchar(arr[i] + '0');
+	i--;
+	}
 
-    free(arr);
+	free(arr);
 
-    return (count);
+	return (count);
 
 }
