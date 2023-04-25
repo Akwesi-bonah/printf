e7568c69c7ebae8a6582d736c06af54efc10706c
rabs23 <ruthrabs22@gmail.com>
2023-04-24 17:37:34 -0700

    update

--- print_hex_P.c
+++ print_hex_P.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_hex - prints value in hexdecimal number
+ * print_hex - a function that prints value in hexdecimal number
  * @arg: argument passed
  * 
  * Return: result
